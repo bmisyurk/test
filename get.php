93fc067ac6add6319b6a22eb78f5a20cbff973dc
Bohdan Misyurko <37878875+bmisyurk@users.noreply.github.com>
2019-05-30 13:48:50 +0300

    Add files via upload

---
+++ get.php
@@ -0,0 +1,57 @@
+<?php
+
+if (empty($_GET['w']) or empty($_GET['h']) or empty($_GET['r'])) {
+    echo 'Заполните все поля';
+}
+else if(is_numeric($_GET['w']) and is_numeric($_GET['h'])and is_numeric($_GET['r']))
+    {
+
+        $width = $_GET['w'];
+        $height = $_GET['h'];
+        $radius = $_GET['r'];
+        $num = $_GET['num'];
+        if($radius > $width/2 or $radius > $height/2)
+            echo 'Радиус превышает допустимые значения';
+        else {
+            $img = imagecreate($width, $height);
+            $background = imagecolorallocate($img, 255, 255, 255);
+            $array = array("blue", "red", "green", "black", "yellow", "blue");
+            $blue   = imagecolorallocate($img, 66, 134, 244);
+            $red = imagecolorallocate($img, 255,   0,   0);
+            $green   = imagecolorallocate($img, 0,   255,   0);
+            $black  = imagecolorallocate($img, 0,   0,   0);
+            $yellow  =imagecolorallocate($img, 255,   255,   0);
+
+            while($num-- > 0)
+            { $cx = rand($radius/2, $width - $radius/2);
+                $cy = rand($radius/2, $height - $radius/2);
+                $col = $array[array_rand($array)];
+                $colour = $$col;
+                imagearc($img,  $cx,  $cy,  $radius,  $radius,  0, 360, $colour);
+            }
+
+            header("Content-type: image/png");
+            imagepng($img);
+            imagepng($img, "newpng.png");
+        }
+    }
+
+else
+    echo 'Введите числовые значения!';
+
+?>
+
+
+<script type="text/javascript">
+    function load() {
+        var w = parseInt('<?php echo $width?>');
+        var h = parseInt('<?php echo $height?>');
+        var r = parseInt('<?php echo $radius?>');
+        if (isNaN(w) || isNaN(h) || isNaN(r)) {
+            document.write("Введите числовые значения!");
+        } else if (r > w / 2 || r > h / 2) {
+            document.write("Радиус превышает допустимые значения!");
+        }
+    }
+</script>
+
