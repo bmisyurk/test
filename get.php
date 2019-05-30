9add07b36c2624facd340a53dbf2cf1a75a69bbc
Bohdan Misyurko <37878875+bmisyurk@users.noreply.github.com>
2019-05-30 15:07:42 +0300

    Add files via upload

--- get.php
+++ get.php
@@ -21,13 +21,12 @@ else if(is_numeric($_GET['w']) and is_numeric($_GET['h'])and is_numeric($_GET['r
             $green   = imagecolorallocate($img, 0,   255,   0);
             $black  = imagecolorallocate($img, 0,   0,   0);
             $yellow  =imagecolorallocate($img, 255,   255,   0);
-
             while($num-- > 0)
-            { $cx = rand($radius/2, $width - $radius/2);
-                $cy = rand($radius/2, $height - $radius/2);
+            { $cx = rand($radius, $width - $radius);
+                $cy = rand($radius, $height - $radius);
                 $col = $array[array_rand($array)];
                 $colour = $$col;
-                imagearc($img,  $cx,  $cy,  $radius,  $radius,  0, 360, $colour);
+                imagearc($img,  $cx,  $cy,  $radius*2,  $radius*2,  0, 360, $colour);
             }
 
             header("Content-type: image/png");
@@ -40,18 +39,3 @@ else
     echo 'Введите числовые значения!';
 
 ?>
-
-
-<script type="text/javascript">
-    function load() {
-        var w = parseInt('<?php echo $width?>');
-        var h = parseInt('<?php echo $height?>');
-        var r = parseInt('<?php echo $radius?>');
-        if (isNaN(w) || isNaN(h) || isNaN(r)) {
-            document.write("Введите числовые значения!");
-        } else if (r > w / 2 || r > h / 2) {
-            document.write("Радиус превышает допустимые значения!");
-        }
-    }
-</script>
-
